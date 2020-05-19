d195e27a038293d28455c6fb20f49c28f47a6356
leroi <harsh.18bcs1049@abes.ac.in>
2020-05-19 14:02:37 +0530

    program 2 of assingment 1

---
+++ assingment/assingment 2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+num = int(input("Enter a number: "))  
+sum = 0  
+temp = num  
+  
+while temp > 0:  
+   digit = temp % 10  
+   sum += digit ** 3  
+   temp //= 10  
+  
+if num == sum:  
+   print(num,"is an Armstrong number")  
+else:  
+   print(num,"is not an Armstrong number")
